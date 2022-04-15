a75f288ead9d640ff2ab52f46be395f77be5d9ae
Rendy Juniarta <rendyjuniarta87@gmail.com>
2022-04-16 00:10:49 +0800

    Import Database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {HomeworkTwo} from './pages/homework-2';
 
 function App() {
+  const secret_api_key=process.env.SPOTIFY_API_KEY;
+  console.log(secret_api_key);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeworkTwo />
     </div>
   );
 }
