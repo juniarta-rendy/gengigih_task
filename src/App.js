ea2349e38ea3258275904f0fef4db3d3775fd736
Rendy Juniarta <rendyjuniarta87@gmail.com>
2022-04-16 23:19:37 +0800

    Homework_Module_3_Session_3

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import { HomeworkOne } from './pages/homework-1';
 
 function App() {
-  const secret_api_key=process.env.SPOTIFY_API_KEY;
-  console.log(secret_api_key);
   return (
     <div className="App">
       <HomeworkOne />
